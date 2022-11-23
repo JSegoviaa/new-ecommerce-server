4b4462f041183d3d30ab9756cd55254329d5d60c
JSegovia <acosta.segoviaa@gmail.com>
2022-11-23 16:10:15 -0500

    se modifica dto

--- src/categories/dto/create-category.dto.ts
+++ src/categories/dto/create-category.dto.ts
@@ -1,7 +1,13 @@
-import { IsNotEmpty, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
 
 export class CreateCategoryDto {
   @IsString()
   @IsNotEmpty()
   title: string;
+
+  @IsBoolean()
+  isActive: boolean;
+
+  @IsBoolean()
+  isPublished: boolean;
 }
--- src/subcategories/dto/create-subcategory.dto.ts
+++ src/subcategories/dto/create-subcategory.dto.ts
@@ -1,4 +1,4 @@
-import { IsNotEmpty, IsPositive, IsString } from 'class-validator';
+import { IsBoolean, IsNotEmpty, IsPositive, IsString } from 'class-validator';
 import { Category } from '../../categories/entities/category.entity';
 
 export class CreateSubcategoryDto {
@@ -8,4 +8,10 @@ export class CreateSubcategoryDto {
 
   @IsPositive()
   category: Category;
+
+  @IsBoolean()
+  isActive: boolean;
+
+  @IsBoolean()
+  isPublished: boolean;
 }
