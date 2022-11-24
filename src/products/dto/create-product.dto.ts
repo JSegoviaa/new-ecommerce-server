6fa4fd8f4d62cbfb75e05eba1ff16d33b2f56e22
JSegovia <acosta.segoviaa@gmail.com>
2022-11-23 20:44:21 -0500

    Se inicia products

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,20 @@
-export class CreateProductDto {}
+import { IsBoolean, IsNotEmpty, IsNumber, IsString } from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsString()
+  @IsNotEmpty()
+  description: string;
+
+  @IsNumber()
+  discount: number;
+
+  @IsBoolean()
+  isPublished: boolean;
+
+  @IsBoolean()
+  isActive: boolean;
+}
--- src/products/entities/product.entity.ts
+++ src/products/entities/product.entity.ts
@@ -1,1 +1,53 @@
-export class Product {}
+import {
+  Column,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryGeneratedColumn,
+} from 'typeorm';
+import * as dayjs from 'dayjs';
+import { User } from '../../users/entities/user.entity';
+
+@Entity({ name: 'products' })
+export class Product {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ nullable: false })
+  title: string;
+
+  @Column({ nullable: false, unique: true })
+  slug: string;
+
+  @Column({ nullable: true })
+  description: string;
+
+  @Column({ default: 0 })
+  discount: number;
+
+  @Column({ type: 'boolean', default: false })
+  isPublished: boolean;
+
+  @Column({ type: 'boolean', default: true })
+  isActive: boolean;
+
+  @ManyToOne(() => User, ({ createdBy }) => createdBy, { eager: true })
+  @JoinColumn({ name: 'createdBy' })
+  createdBy: User;
+
+  @ManyToOne(() => User, ({ updatedBy }) => updatedBy, { eager: true })
+  @JoinColumn({ name: 'updatedBy' })
+  updatedBy: User;
+
+  @Column({
+    type: 'timestamp',
+    default: dayjs().format(),
+  })
+  createdAt: string;
+
+  @Column({
+    type: 'timestamp',
+    default: dayjs().format(),
+  })
+  updatedAt: string;
+}
